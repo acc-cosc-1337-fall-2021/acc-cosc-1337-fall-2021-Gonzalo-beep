aad8d883bb4b20ca38c6047a354e28db778c41d3
Gonzalo-beep <gonzalo.reyes@g.austincc.edu>
2021-09-10 12:37:03 -0500

    Homework 01 Completed

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,15 +1,28 @@
 ﻿//write include statements
+#include "data_types.h"
+#include<iostream>
 
 
 //write namespace using statement for cout
-
+using std::cout;
+using std::cin;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
-	
+	int num;
+    cout<<"Please enter a number: ";
+    cin >> num;
+    int result; 
+    result = multiply_numbers(num);
+    cout<<result<<std::endl;
+
+    int num1 = 4;
+    result = multiply_numbers(num1);
+    cout<<result<<std::endl;
 
 	return 0;
 }
+   
